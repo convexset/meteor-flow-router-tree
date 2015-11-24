fd9814a612e61f4a77b07eb289ac0fe9c1778fc6
Jeremy Chen <convexset@gmail.com>
2015-11-24 23:18:18 +0800

    Dependency update (to deal with minification problem)

--- packages/flow-router-tree/package.js
+++ packages/flow-router-tree/package.js
@@ -1,6 +1,6 @@
 Package.describe({
 	name: 'convexset:flow-router-tree',
-	version: '0.1.0',
+	version: '0.1.1',
 	summary: 'A tool for facilitating the maintenance of FlowRouter routes cleanly.',
 	git: 'https://github.com/convexset/meteor-flow-router-tree',
 	documentation: '../../README.md'
@@ -14,7 +14,7 @@ Package.onUse(function(api) {
 		'check',
 		'kadira:flow-router@2.6.2',
 		'ramda:ramda@0.17.1',
-		'convexset:package-utils@0.1.0',
+		'convexset:package-utils@0.1.3',
 		'convexset:match-extensions@0.1.0'
 	]);
 	api.addFiles(['flow-router-tree.js']);
