4a4a5fa8c8293342f0f227306236f1f979d3cae8
Jeremy Chen <convexset@gmail.com>
2016-08-10 10:53:20 +0800

    updated version of access-check

--- packages/flow-router-tree/package.js
+++ packages/flow-router-tree/package.js
@@ -1,7 +1,7 @@
 Package.describe({
 	// [validatis:stack]
 	name: 'convexset:flow-router-tree',
-	version: '0.1.4_4',
+	version: '0.1.4_5',
 	summary: 'A tool for facilitating the maintenance of FlowRouter routes cleanly.',
 	git: 'https://github.com/convexset/meteor-flow-router-tree',
 	documentation: '../../README.md'
@@ -15,7 +15,7 @@ Package.onUse(function(api) {
 		'session',
 		'kadira:flow-router@2.12.1',
 		'kadira:blaze-layout@2.3.0',
-		'convexset:access-check@0.1.0_1',
+		'convexset:access-check@0.1.1_7',
 		'tmeasday:check-npm-versions@0.3.1'
 	]);
 	api.addFiles(['flow-router-tree.js']);
